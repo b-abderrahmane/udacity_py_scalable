ee43a0a2f8a49fe0a283a0c46f9c17d965935428
Boulgheraif Abderrahmane <b-abderrahmane@outlook.com>
2016-09-15 20:38:20 +0100

    Lesson 3 commit

--- Lesson_3/00_Conference_Central/settings.py
+++ Lesson_3/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '644031030182-lu8rdmcar9ei6co5upo4vagst45r47d8.apps.googleusercontent.com'
 
